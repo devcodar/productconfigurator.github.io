0585b6f4f1eb5262a87cb502841be04cb49eb095
devcodar <sdanke48@gmail.com>
2024-06-27 10:15:53 -0300

    Readme
    Atualização

--- img.js
+++ img.js
@@ -1,14 +1,20 @@
 document.getElementById('ativarSkybox').addEventListener('click', function() {
-    const modelo3D = document.getElementById('modelo2D');
-    const caminhoImagemHDR = 'kloppenheim_02_4k.hdr';
+  const modelo3D = document.getElementById('modelo2D');
+  const caminhoImagemHDRPrincipal = 'rosendal_plains_2_2k.hdr';
+  const caminhoImagemHDRSecundario = 'kloppenheim_02_4k.hdr';
+  const skyboxAtivo = modelo3D.getAttribute('skybox-image');
   
-    // Ativa o skybox HDR
-    modelo3D.setAttribute('skybox-image', caminhoImagemHDR);
-    modelo3D.setAttribute('scale', '1.0 1.0 1.0 ');
-    modelo3D.setAttribute ('environment-image', caminhoImagemHDR);
+  // Alterna entre o HDR principal e o secundário
+  if (skyboxAtivo === caminhoImagemHDRSecundario) {
+    modelo3D.setAttribute('skybox-image', caminhoImagemHDRPrincipal);
+    modelo3D.setAttribute('environment-image', caminhoImagemHDRPrincipal);
+  } else {
+    modelo3D.setAttribute('skybox-image', caminhoImagemHDRSecundario);
+    modelo3D.setAttribute('environment-image', caminhoImagemHDRSecundario);
+  }
 
-    // Opcional: Ative a sombra e a iluminação do modelo baseada no ambiente HDR
-    modelo3D.setAttribute('shadow-intensity', '5');
-  });
+  modelo3D.setAttribute('scale', '1.0 1.0 1.0 ');
+  modelo3D.setAttribute('shadow-intensity', '5');
+});
   
 
