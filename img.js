04fcfc833c56b9ccb4bea3af766a6e5464e35e1d
devcodar <sdanke48@gmail.com>
2024-06-27 01:30:46 -0300

    Readme
    Atualizacao de scale

--- img.js
+++ img.js
@@ -4,7 +4,8 @@ document.getElementById('ativarSkybox').addEventListener('click', function() {
   
     // Ativa o skybox HDR
     modelo3D.setAttribute('skybox-image', caminhoImagemHDR);
-    modelo3D.setAttribute('scale', '0.3 0.3 0.3 ');
+    modelo3D.setAttribute('scale', '1.0 1.0 1.0 ');
+    modelo3D.setAttribute ('environment-image', caminhoImagemHDR);
 
     // Opcional: Ative a sombra e a iluminação do modelo baseada no ambiente HDR
     modelo3D.setAttribute('shadow-intensity', '5');
