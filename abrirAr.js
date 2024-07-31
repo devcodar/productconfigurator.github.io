236db5bae673bcba08c0dba310eabe2501b5c7a2
devcodar <sdanke48@gmail.com>
2024-07-30 22:16:44 -0300

    Readme
    Att

---
+++ abrirAr.js
@@ -0,0 +1,13 @@
+document.addEventListener('DOMContentLoaded', function() {
+    const modelo3D = document.getElementById('modelo2D');
+    const abrirAR = document.getElementById('abrirAR');
+  
+    abrirAR.addEventListener('click', function() {
+      if (modelo3D && modelo3D.activateAR) {
+        modelo3D.activateAR();
+      } else {
+        console.error('A função activateAR não está disponível no elemento model-viewer.');
+      }
+    });
+  });
+  
--- img.js
+++ img.js
@@ -1,22 +1,35 @@
-document.getElementById('ativarSkybox').addEventListener('click', function() {
-  const modelo3D = document.getElementById('#modelo2D');
+  const ativarSkybox = document.getElementById('ativarSkybox').addEventListener('click', function() {
+  const modelo2D = document.getElementById('#modelo2D');
   const caminhoImagemHDRPrincipal = 'rosendal_plains_2_2k.hdr';
   const caminhoImagemHDRSecundario = 'kloppenheim_02_4k.hdr';
   const skyboxAtivo = modelo3D.getAttribute('skybox-image');
   
   // Alterna entre o HDR principal e o secundário
   if (skyboxAtivo === caminhoImagemHDRSecundario) {
-    modelo3D.setAttribute('skybox-image', caminhoImagemHDRPrincipal);
-    modelo3D.setAttribute('environment-image', caminhoImagemHDRPrincipal);
+    modelo2D.setAttribute('skybox-image', caminhoImagemHDRPrincipal);
+    modelo2D.setAttribute('environment-image', caminhoImagemHDRPrincipal);
   } else {
-    modelo3D.setAttribute('skybox-image', caminhoImagemHDRSecundario);
-    modelo3D.setAttribute('environment-image', caminhoImagemHDRSecundario);
+    modelo2D.setAttribute('skybox-image', caminhoImagemHDRSecundario);
+    modelo2D.setAttribute('environment-image', caminhoImagemHDRSecundario);
   }
-  modelo3D.setAttribute('shadow-intensity','0');
-  modelo3D.setAttribute('shadow-softness', '5');
-  modelo3D.setAttribute('skybox-height','1');
-  modelo3D.setAttribute('exposure','2');
+  modelo2D.setAttribute('shadow-intensity','0');
+  modelo2D.setAttribute('shadow-softness', '5');
+  modelo2D.setAttribute('skybox-height', '2rem');
+  modelo2D.setAttribute('exposure','2');
+
+  // Esconde o botão e exibe o modelo 3D
+  document.body.style.display = 'flex';
+  document.body.style.height = '100vh';
+  document.body.style.width = '100%';
+  document.body.style.alignItems = 'center';
+  document.body.style.justifyContent = 'center';
 });
   
 
+
+
+  
+  
+  
+ 
 
